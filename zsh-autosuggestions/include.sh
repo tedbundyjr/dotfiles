53ad03edd0473dae0043bced8432e601163939f8
Haris Subandie MS <haris@vms.my>
2021-06-26 22:44:45 +0800

    zsh autosuggestion

---
+++ zsh-autosuggestions/include.sh
@@ -0,0 +1,4 @@
+if test -f "/usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+then 
+    source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+fi
---
+++ zsh-autosuggestions/install.sh
@@ -0,0 +1,4 @@
+if test ! -f "/usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+then
+    brew install zsh-autosuggestions
+fi
