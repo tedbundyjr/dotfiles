0699d5a1dc052a7ff8f50ce63ea5ed0338e7058f
Haris Subandie Md Suhaimin <haris@vms.my>
2021-08-03 02:08:43 +0800

    working zsh

--- zsh-autosuggestions/completion.zsh
+++ zsh-autosuggestions/completion.zsh
@@ -1,9 +1,8 @@
 # Uses git's autocompletion for inner commands. Assumes an install of git's
 # bash `git-completion` script at $completion below (this is where Homebrew
 # tosses it, at least).
-completion='$(brew --prefix)/share/zsh/site-functions/_git'
+completion="$(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
 
-if test -f $completion
-then
+if [[ -a "$completion" ]]; then
   source $completion
 fi
--- zsh-autosuggestions/include.sh
+++
@@ -1,4 +0,0 @@
-if test -h "/usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
-then 
-    source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-fi
