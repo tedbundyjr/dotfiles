18710afb4ae59d8ce2b85248c722db2376826cee
Haris Subandie Md Suhaimin <haris@vms.my>
2021-11-25 21:09:04 +0800

    update dotfiles

--- valet-apache-switcher/aliases.zsh
+++ valet-apache-switcher/aliases.zsh
@@ -1,2 +1,2 @@
-alias v2a='valet stop && sudo apachectl start'
-alias a2v='sudo apachectl stop && valet start'
+alias v2a="valet stop && sudo apachectl start && ps aux |egrep 'httpd|nginx'"
+alias a2v="sudo apachectl stop && valet start && ps aux |egrep 'httpd|nginx'"
